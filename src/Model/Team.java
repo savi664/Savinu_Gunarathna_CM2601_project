e218341930c352d142751f160ead2b499e75f4f2
savi664 <savinugunarathna4@gmail.com>
2025-10-03 01:02:37 +0530

    Creating the Team class

--- src/Model/Team.java
+++ src/Model/Team.java
@@ -1,6 +1,33 @@
 package Model;
 
+import java.util.List;
+
 public class Team {
+     private Integer team_id;
+     private List<Participant> participants;
 
+    public Team(Integer team_id, List<Participant> participants) {
+        this.team_id = team_id;
+        this.participants = participants;
+    }
 
+    public Integer getTeam_id() {
+        return team_id;
+    }
+
+    public void setTeam_id(Integer team_id) {
+        this.team_id = team_id;
+    }
+
+    public List<Participant> getParticipants() {
+        return participants;
+    }
+
+    public Boolean is_balanced(){
+        return false;
+    }
+
+    public void setParticipants(List<Participant> participants) {
+        this.participants = participants;
+    }
 }
